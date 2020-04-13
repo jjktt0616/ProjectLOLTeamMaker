738235858f0ef8268776c855d5574ade0539643f
jjdh0616 <63234597+jjdh0616@users.noreply.github.com>
2020-04-13 17:49:11 +0900

    Update bottest.py

--- bottest.py
+++ bottest.py
@@ -1,7 +1,7 @@
 import discord, asyncio # 디스코드 모듈과, 보조 모듈인 asyncio를 불러옵니다.
 import datetime
 
-token = "Njk3NzYxOTgxMzg1NDA4NTgz.Xo7_kQ.T-pMZMgwX8B2e_0TCExV8IZF0eA" # 아까 메모해 둔 토큰을 입력합니다
+token = "Njk3NzYxOTgxMzg1NDA4NTgz.XpQk5w.mdDxFN-KJYnvNOZWfUHJ1G3JKVQ" # 아까 메모해 둔 토큰을 입력합니다
 client = discord.Client() # discord.Client() 같은 긴 단어 대신 client를 사용하겠다는 선언입니다.
 
 
